1daa9c73c6d60f793e4c960baec7c88b1ecf9beb
Mao Zhang <mao.zhang233@gmail.com>
2023-02-20 20:57:11 -0600

    create application object and run

--- src/playground.cc
+++ src/playground.cc
@@ -8,17 +8,29 @@
  * @copyright Copyright (c) 2023
  *
  */
+#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 
+#include "application.h"
+
 int main(int argc, char* argv[]) {
-  std::clog << "----- Starting: Hello -----" << std::endl;
+  std::clog << "----- Playground: Starting -----" << std::endl;
 
   std::clog << "----- Arguments Count: " << argc << " -----" << std::endl;
   for (int i = 0; i < argc; ++i) {
     std::clog << "----- Arguments No." << i + 1 << ": " << argv[i] << std::endl;
   }
 
-  std::clog << "----- Terminating: Bye -----" << std::endl;
+  try {
+    playground::Config config{};
+    playground::Application app{config};
+
+    app.Run();
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
   return EXIT_SUCCESS;
 }
