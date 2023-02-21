96e59bc22ab8a7552ea05067876a7e10439bd200
Mao Zhang <mao.zhang233@gmail.com>
2023-02-20 22:07:22 -0600

    add shader compiling script on MacOS

---
+++ shaders/compile.sh
@@ -0,0 +1,2 @@
+/Users/mao/Documents/Dev/VulkanSDK/1.3.231.0/macOS/bin/glslc ./triangle.vert -o ./triangle.vert.spv
+/Users/mao/Documents/Dev/VulkanSDK/1.3.231.0/macOS/bin/glslc ./triangle.frag -o ./triangle.frag.spv
